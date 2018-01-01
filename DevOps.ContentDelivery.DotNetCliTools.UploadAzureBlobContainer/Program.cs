ecd67343c978bcc966e8ea08abed571904b2695c
Christopher Dorst <chris@dorst.email>
2018-01-01 01:59:15 -0600

    Add program

---
+++ DevOps.ContentDelivery.DotNetCliTools.UploadAzureBlobContainer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using static Common.Functions.BuildCommandLineArgConfiguration.CommandLineConfigurationBuilder;
+using static DevOps.ContentDelivery.Functions.UploadDirectoryToAzureBlobContainer.DirectoryUploader;
+
+namespace DevOps.ContentDelivery.DotNetCliTools.UploadAzureBlobContainer
+{
+    public static class Program
+    {
+        private const string Container = nameof(Container);
+        private const string Content = nameof(Content);
+
+        public static async Task Main(string[] args)
+            => await UploadFiles(
+                ContainerName(args),
+                path: Content);
+
+        private static string ContainerName(string[] args)
+            => Configuration(args)[Container];
+    }
+}
